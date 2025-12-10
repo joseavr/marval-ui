5d7875db2c7fcabaec5c81986543dfbaf94c1c39
joseavr <jvaldiv8@montgomerycollege.edu>
2025-12-09 21:35:52 -0500

    chore: refactor component names

--- apps/v1/src/app/docs/layout.tsx
+++ apps/v1/src/app/docs/layout.tsx
@@ -1,13 +1,13 @@
 import { DocsSideBar } from "@/components/pages/docs/sidebar"
-import { Footer } from "@/components/shared/footer"
-import { Header } from "@/components/shared/header"
+import { AppFooter } from "@/components/shared/app-footer"
+import { AppHeader } from "@/components/shared/app-header"
 import { Container } from "@/components/shared/page-container"
 import { RootLayout } from "@/components/shared/root-layout"
 
 export default function DocsLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<RootLayout>
-			<Header />
+			<AppHeader />
 			{/* <div className="h-4 w-full"></div> */}
 			<div className="flex flex-1 flex-col">
 				<Container className="relative flex items-start">
@@ -17,7 +17,7 @@ export default function DocsLayout({ children }: { children: React.ReactNode }) 
 					</main>
 				</Container>
 			</div>
-			<Footer />
+			<AppFooter />
 		</RootLayout>
 	)
 }
--- apps/v1/src/components/shared/app-header.tsx
+++ apps/v1/src/components/shared/app-header.tsx
@@ -3,15 +3,17 @@ import Link from "next/link"
 import { Search } from "@/components/pages/docs/search"
 import { Container } from "@/components/shared/page-container"
 
-export function Header() {
+export function AppHeader() {
 	return (
 		<header className="sticky top-0 z-30 w-full bg-transparent backdrop-blur-sm">
 			<Container>
-				<div className="flex h-(--header-height) items-center justify-between">
-					<Link href="/" className="font-bold text-xl">
-						marval ui
-					</Link>
-					<Search />
+				<div className="h-(--header-height)">
+					<div className="flex h-8 items-start justify-between pt-3">
+						<Link href="/" className="h-8 font-bold text-xl">
+							marval ui
+						</Link>
+						<Search />
+					</div>
 				</div>
 			</Container>
 		</header>
