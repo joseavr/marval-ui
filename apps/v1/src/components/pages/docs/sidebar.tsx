7389454971090e1e8338feaa858bf427ec045222
joseavr <jvaldiv8@montgomerycollege.edu>
2025-12-11 00:50:52 -0500

    chore: enhance ScrollArea component with hideScrollbar prop and update sidebar styling

--- apps/v1/src/components/pages/docs/sidebar.tsx
+++ apps/v1/src/components/pages/docs/sidebar.tsx
@@ -22,7 +22,7 @@ export function DocsSideBar() {
 			data-id="docs-leftside"
 			className="sticky top-(--header-height) flex h-[calc(100svh-var(--header-height)-var(--footer-height))] min-w-(--sidebar-width) max-w-(--sidebar-width) flex-1 flex-col pt-4 text-sm"
 		>
-			<ScrollArea>
+			<ScrollArea hideScrollbar>
 				<nav
 					className={cn("flex flex-col gap-8", layout === "zen" && "invisible")}
 					style={
